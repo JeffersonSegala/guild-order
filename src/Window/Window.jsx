7db1336c474b8a392e6483409df852ddf6ce6e99
Jeff <beperson@gmail.com>
2024-03-15 15:25:06 -0300

    opne close on name click

--- src/Window/Window.jsx
+++ src/Window/Window.jsx
@@ -6,8 +6,8 @@ const Window = ({ children, title, id, isOpen, onClose, onEdit }) => {
   
   return (
     <div className="window" id={id} >
-      <div className='window__headers' >
-        <div>{title}</div>
+      <div className='window__headers'>
+        <div className='window__pointer' onClick={() => setOpen(open => !open)} >{title}</div>
         <div className='window__actions'>
           <img src={open ? 'minusIcon.png' : 'plusIcon.png'} className="window__header-icon" alt="" onClick={() => setOpen(open => !open)} /> 
           {onEdit && 
