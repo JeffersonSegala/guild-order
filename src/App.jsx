57f3c86cf03a55c03ba8cc00acf0727b73fc085f
Jefferson Segala <jefferson.segala@involves.com>
2024-06-28 10:01:04 -0300

    Remoção das funcionalidades de aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import OnlineMembers from './OnlineMembers/OnlineMembers';
-import Parties from './Party/Parties';
 import Header from './Components/Header/Header';
-import Constants from './Constants';
+import Window from './Window/Window';
 
 function App() {
   const [guild, setGuild] = useState([]);
-  const [lsUserKey, setLsUserKey] = useState('');
-  const [user, setUser] = useState('');
 
   useEffect(() => {
     fetchOrder();
     setInterval(fetchOrder, 60000);
-
-    fetchUser();
   }, []);
 
   const fetchOrder = () => {
@@ -24,33 +19,36 @@ function App() {
         setGuild(data.guild.members)
     });
   }
-
-  const fetchUser = () => {
-    let lsUserKey = localStorage.getItem("userKey");
-    if (!lsUserKey) {
-      lsUserKey = new Date().getTime();
-      localStorage.setItem("userKey", lsUserKey)
-    }
-    setLsUserKey(lsUserKey)
-
-    fetch(Constants.API_URL + '/user/' + lsUserKey)
-      .then(response => response.json())
-      .then(data => {
-        setUser(data)
-    });
-  }
-  
+ 
   return (
     <div className="App">
             
-      <Header lsUserKey={lsUserKey} user={user} />
+      <Header />
 
       <div className="flex-container">
 
         <OnlineMembers members={guild} />
 
-        <Parties players={guild} user={user} />   
-       
+        <Window title={'[28/06/24 - 19:30] GT FINAL > FERUMBRAS FINAL'} >
+          <div>
+            Concentração na entrada da GT para formação da PT, as 19:20 as vagas remanecentes da PT serão ofertadas no Advertising.
+          </div>
+          <br/>
+          <div>Inscrições nos eventos do discord</div>
+          <div>
+            <img src={'guia1.png'} />
+            <img src={'guia2.png'} />
+          </div>
+        </Window>
+
+        <Window title={'[30/06/24 - 10:30 > 14:30 > 18:30] Warzone 4'} >
+          <div>Inscrições nos eventos do discord</div>
+          <div>
+            <img src={'guia1.png'} />
+            <img src={'guia3.png'} />
+          </div>
+        </Window>
+      
       </div>
 
     </div>
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,31 +1,11 @@
-import Constants from '../../Constants';
 import './style.css';
 
-const Header = ({lsUserKey, user}) => {
- 
-  const shareParties = () => {
-    fetch(Constants.API_URL + '/parties/share')
-      .then(response => response.text())
-      .then(data => {
-        navigator.clipboard.writeText(data)
-    });
-  }
+const Header = () => {
 
   return (
     <>
-    <div className='user'>
-      {lsUserKey+'.'+ (user?.id||'?') + (user.logout ? '.ADM' : '')}
-    </div>
     <div className='header'>
-      <div className='rules'>
-        {user.logout && <img src={'secretInstruction.gif'} title="Regras/Infos" />}
-      </div>
-      <div>
         <img src={'logoNname.webp'} className="logo" alt='logo' />
-      </div>
-      <div className='rules'>
-        {user.logout && <img src={'letter.gif'} onClick={shareParties} title="Copiar eventos" />}
-      </div>
     </div>
     </>
   );
